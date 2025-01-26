3d4a8c17a7ab57a118bbad503b08b33507f67572
Louis Appleyard <louisappleyard@icloud.com>
2025-01-26 14:13:27 +0000

    added client side javascript, added first test fetch, added title text and paragraph text to homepage, attempted to centre button correctly

---
+++ client/index.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('bookgigbutton').addEventListener('click', async function(event) {
+        try {
+            let response = await fetch('http://127.0.0.1:8090/list');
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            let body = await response.text();
+            document.getElementById('content').innerHTML = body;
+        } catch (error) {
+            console.error('Fetch error:', error);
+        }
+    });
+});
--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
+
+app.use(cors())
 
 app.use(express.static('client'));
 
 app.get('/', function(req, resp){
   resp.send('Hello world')
 })
+
+let instruments = [ 'piano', 'concertina', 'double bass'];
+
+app.get('/list', function (req, resp){
+    resp.send(instruments);
+});
 
 app.listen(8090)
